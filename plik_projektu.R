815f987bd3925f8357a3c14a6d2d3fe79bb313f1
student <student@A413-04.woiz.polsl.local>
2022-05-10 11:38:29 +0200

    pierwszy commit

---
+++ plik_projektu.R
@@ -0,0 +1,15 @@
+library(dplyr)
+#wczytywanie danych
+df <- readxl::read_excel(path = "./Dane-Nowak-Okoń.xlsx")
+
+#poprawa kolumn
+df <- df %>% janitor::clean_names()
+
+#sprawdzenie kolumn
+df %>% glimpse()
+df %>% as_tibble()
+
+#zmiennosc
+cv <- function(x){(sd(x)/mean(x))*100}
+sapply(as.list(df[,-1]), cv)
+
